5212f2878a6fa9f1c79d5c425684d58b74bc66e0
saurav <sauravkul0@gmail.com>
2022-05-13 19:15:00 +0530

    Add dynamic function implemeted

--- script.js
+++ script.js
@@ -1,7 +1,13 @@
 const addBtn = document.querySelector(".add-btn");
 const modalCont = document.querySelector(".modal-cont");
-
+let color = ['lightpink' , 'lightgreen' , 'lightblue' , 'black'];
+let modalPriorityColor = color[color.length - 1];
+let allPriorityColors = document.querySelectorAll(".priority-color");
+let textAreaCont =  document.querySelector(".modal-Text");
+let mainCont = document.querySelector(".main-cont");
 let isModalPresent  = false;
+
+
 addBtn.addEventListener('click' ,  function(){
  if(!isModalPresent){
      modalCont.style.display = "flex";
@@ -11,14 +17,42 @@ addBtn.addEventListener('click' ,  function(){
  isModalPresent = !isModalPresent;
 })  
 
-let allPriorityColors = document.querySelectorAll(".priority-color");
 allPriorityColors.forEach(function(colorElement){
     colorElement.addEventListener('click' , function(){
         allPriorityColors.forEach(function(priorityColorElement){
         priorityColorElement.classList.remove("active");
     });
     colorElement.classList.add("active");
-  
+    modalPriorityColor = colorElement.classList[0];
+
 });
 });
+
+modalCont.addEventListener("keydown" , function(e){
+    let key  = e.key;
+    if(key == "Shift"){
+     createTicket(modalPriorityColor , textAreaCont.value);
+
+     modalCont.style.display = "none";
+     isModalPresent = false;
+     modalPriorityColor = colorElement.classList[1];
+    }
+});
+
+
+function createTicket( ticketColor , data){
+    let ticketCont  = document.createElement("div");
+    ticketCont.setAttribute("class" , "ticket-cont");
+    ticketCont.innerHTML = `
+    <div class="ticket-color ${ticketColor}" ></div>
+    <div class="ticket-id"></div>
+    <div class="task-area ">${data}</div>
+    `;
+    mainCont.appendChild(ticketCont);
+    textAreaCont.value= "";
+}
+
+
+    
+
 
